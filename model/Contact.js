6d39d8d4deb769d4d6bf10d62379b67fb66a141c
Vuthy <suonx002@gmail.com>
2019-11-30 00:03:12 -0600

    add auth to private contact routes

---
+++ model/Contact.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ContactSchema = Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  phone: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('contact', ContactSchema);
